d25cc26ea9dcb2b38763155c922714dd0911b8db
KLM <91488137+planeklm@users.noreply.github.com>
2022-10-20 20:45:48 +0100

    Qb-target support

--- client.lua
+++ client.lua
@@ -1,1 +1,18 @@
 local QBCore = exports['qb-core']:GetCoreObject()
+local toilets = Config.Toilets
+
+
+
+ 
+ exports['qb-target']:AddTargetModel(toilets, {
+    options = {
+    {
+      type = "client",
+      event = "razed-toilets:useToliet",
+      icon = Target.IconsHighLight["smallBins"],
+      label = Target.Label["smallBins"],
+      targeticon = Target.Icon["smallBins"],
+    }
+  },
+  distance = Config.TargetDistance,
+})
