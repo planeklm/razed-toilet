4145c6a30f88bdaffa6f7743e363634f7e68ffff
KLM <91488137+planeklm@users.noreply.github.com>
2022-10-21 18:27:26 +0100

    Added props and stress level customization.

--- config.lua
+++ config.lua
@@ -3,6 +3,11 @@ Config = {}
 Config.Status = '^5Version 1.0 - Up to date!'
 Config.TargetDistance = 1.5 -- 3rd Eye target distance
 
+Config.relieveStress = 15
+Config.gainStress = 10
+
 Config.Toilets = {
-    `a_m_m_business_01`,
+    `prop_ld_toilet_01`,
+    `prop_toilet_02`,
+    `prop_toilet_01`,
 }
