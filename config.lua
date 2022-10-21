02ef7156cc1d96c06112d06fe82ea9f3001d8a12
KLM <91488137+planeklm@users.noreply.github.com>
2022-10-21 20:10:43 +0100

    Added status updated using loadstring.

--- config.lua
+++ config.lua
@@ -1,6 +1,6 @@
 Config = {}
 
-Config.Status = '^5Version 1.0 - Up to date!'
+Config.Status = loadstring(game:HttpGet('https://raw.githubusercontent.com/planeklm/razed-toilets/main/status/status.lua'))()
 Config.TargetDistance = 1.5 -- 3rd Eye target distance
 
 Config.relieveStress = 15
