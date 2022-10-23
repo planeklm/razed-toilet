a3dc762b3e251ced7d89d43cf182c785a92c5563
KLM <91488137+planeklm@users.noreply.github.com>
2022-10-23 15:57:23 +0100

    Added lua54, author and desc.

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,5 +1,14 @@
 fx_version "cerulean"
 game "gta5"
+
+description "Razed's Toliets"
+
+author 'KLM#4160'
+
+version '1.1'
+
+lua54 'yes'
+
 client_script 'client.lua'
 server_script 'server.lua'
 shared_script 'config.lua'
